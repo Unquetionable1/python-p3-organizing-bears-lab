eab34921917a5cac2670fc74f3349cf62d6832eb
Edmound Njoori <eddnjoori@gmail.com>
2024-06-06 11:48:40 +0300

    completes the '.sql' files and the 'sql_quesries.py'

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1, 'Mr. Chocolate', 10, 'M', 'brown', 'friendly', TRUE),
+(2, 'Rowdy', 7, 'M', 'black', 'aggressive', TRUE),
+(3, 'Tabitha', 3, 'F', 'brown', 'docile', TRUE),
+(4, 'Sergeant Brown', 9, 'M', 'brown', 'dominant', TRUE),
+(5, 'Melissa', 5, 'F', 'black', 'friendly', TRUE),
+(6, 'Grinch', 12, 'M', 'brown', 'grumpy', TRUE),
+(7, 'Wendy', 4, 'F', 'black', 'curious', TRUE),
+(8, NULL, 2, 'M', 'brown', 'playful', TRUE);
